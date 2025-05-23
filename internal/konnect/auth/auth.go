e1a4bf0efa72095b613d557a3544c86684fb0b2b
Rick Spurgeon <rspurgeon@users.noreply.github.com>
2025-05-22 21:54:59 -0500

    fix(auth): organize imports according to gci standards
    Group imports into standard library, third-party, Kong SDK, and local packages

--- internal/konnect/auth/auth.go
+++ internal/konnect/auth/auth.go
@@ -14,12 +14,14 @@ import (
 	"strings"
 	"time"
 
+	"github.com/ajg/form"
+	"github.com/google/uuid"
+
 	kkInternal "github.com/Kong/sdk-konnect-go-internal"
 	kkInternalComps "github.com/Kong/sdk-konnect-go-internal/models/components"
 	kk "github.com/Kong/sdk-konnect-go" // kk = Kong Konnect
 	kkComps "github.com/Kong/sdk-konnect-go/models/components"
-	"github.com/ajg/form"
-	"github.com/google/uuid"
+
 	"github.com/kong/kongctl/internal/config"
 )
 
