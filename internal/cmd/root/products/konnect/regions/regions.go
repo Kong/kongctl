17a6d8d0e666dc2f1b48df18d31367ad8f1e4d82
Rick Spurgeon <10521262+rspurgeon@users.noreply.github.com>
2025-11-13 20:30:59 -0600

    Feat: Add Konnect region flag + tooling, region scenario, and multi-region reset
      - Introduce konnect.region flag/config across CLI commands with automatic base URL resolution, docs/tests, and a get regions helper command.
      - Add e2e region scenario (US/EU overlays) plus cleanup logic, and extend reset harness to accept resetOrgRegions so org wipes cover multiple regions.

---
+++ internal/cmd/root/products/konnect/regions/regions.go
@@ -0,0 +1,51 @@
+package regions
+
+import (
+	"fmt"
+
+	"github.com/kong/kongctl/internal/cmd/root/verbs"
+	"github.com/kong/kongctl/internal/meta"
+	"github.com/kong/kongctl/internal/util/i18n"
+	"github.com/kong/kongctl/internal/util/normalizers"
+	"github.com/spf13/cobra"
+)
+
+const (
+	CommandName = "regions"
+)
+
+var (
+	regionsUse = CommandName
+
+	regionsShort = i18n.T("root.products.konnect.regions.regionsShort",
+		"List available Konnect regions")
+
+	regionsLong = normalizers.LongDesc(i18n.T("root.products.konnect.regions.regionsLong",
+		`The regions command lists the Konnect regions that can be used for regional API requests.`))
+
+	regionsExample = normalizers.Examples(i18n.T("root.products.konnect.regions.regionsExample",
+		fmt.Sprintf(`
+	# List Konnect regions
+	%[1]s get regions
+	`, meta.CLIName)))
+)
+
+func NewRegionsCmd(
+	verb verbs.VerbValue,
+	addParentFlags func(verbs.VerbValue, *cobra.Command),
+	parentPreRun func(*cobra.Command, []string) error,
+) (*cobra.Command, error) {
+	baseCmd := cobra.Command{
+		Use:     regionsUse,
+		Short:   regionsShort,
+		Long:    regionsLong,
+		Example: regionsExample,
+	}
+
+	switch verb {
+	case verbs.Get:
+		return newGetRegionsCmd(verb, &baseCmd, addParentFlags, parentPreRun).Command, nil
+	default:
+		return &baseCmd, nil
+	}
+}
