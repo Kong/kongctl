e5b087dff51d147cdb5e07c17dfa2675c453b0df
Prashansa Kulshrestha <prashansa.kulshrestha@konghq.com>
2026-01-29 16:19:55 +0530

    feat: added dump command for teams

--- internal/cmd/root/verbs/dump/common.go
+++ internal/cmd/root/verbs/dump/common.go
@@ -104,6 +104,8 @@ func mapResourceName(name string) string {
 		return "application_auth_strategies"
 	case "control-plane", "controlplane", "controlplanes", "control_planes":
 		return "control_planes"
+	case "teams", "team":
+		return "teams"
 	default:
 		return name
 	}
