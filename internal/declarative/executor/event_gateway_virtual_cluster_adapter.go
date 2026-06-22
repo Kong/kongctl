3ea3d6eea7f79bfe7e1c7ccd52a142c2dc377746
Rick Spurgeon <10521262+rspurgeon@users.noreply.github.com>
2026-06-22 14:19:26 -0500

    WIP: internal sdk usage and changes required

--- internal/declarative/executor/event_gateway_virtual_cluster_adapter.go
+++ internal/declarative/executor/event_gateway_virtual_cluster_adapter.go
@@ -829,7 +829,7 @@ func buildVirtualClusterNamespace(field any) (*kkComps.VirtualClusterNamespace, 
 	}
 
 	namespace := &kkComps.VirtualClusterNamespace{
-		Mode: kkComps.Mode(mode),
+		Mode: kkComps.VirtualClusterNamespaceMode(mode),
 	}
 
 	if prefix, ok := nsMap["prefix"].(string); ok {
